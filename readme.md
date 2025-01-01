30aa56f821197189ad1542bfd1c8dc946b8d9f81
SeukWon Kang <kasworld@gmail.com>
2025-01-01 12:31:04 +0900

    문서 추가

---
+++ readme.md
@@ -0,0 +1,5 @@
+# 예능/놀이용 룰렛 
+
+![alt text](doc/screenshot1.png)
+
+![alt text](doc/screenshot2.png)
--- roulette.gd
+++ roulette.gd
@@ -2,7 +2,7 @@ extends Node3D
 class_name Roulette
 
 func init(r :float, d :float, fsize :float) -> void:
-	var plane = Global3d.new_cylinder(d, r,r, Global3d.get_color_mat(Color.GREEN ) )
+	var plane = Global3d.new_cylinder(d, r,r, Global3d.get_color_mat(Color.DARK_GREEN ) )
 	plane.position.y = -d
 	add_child(plane)
 
@@ -10,15 +10,17 @@ func init(r :float, d :float, fsize :float) -> void:
 	for i in n:
 		var unit = 360.0/n
 		var deg = unit * i
-		add_bar(deg, r*0.07, r*0.99, d/10, d)
-		deg = unit * i + unit/2
-		add_text(deg, r*0.9, r/10, d, "%d" %i)
+		add_bar(deg, r*0.5, r, d/10, d)
+		var co = Color.RED
+		if i % 2 == 0:
+			co = Color.BLUE
+		add_text(deg+ unit/2, r*0.9, r/10, d, co, "%d" % [ i * 25 % n ] )
 
-	var cc = Global3d.new_cylinder(d,r*0.02,r*0.02, Global3d.get_color_mat(Color.BLUE))
+	var cc = Global3d.new_cylinder(d,r*0.04,r*0.04, Global3d.get_color_mat(Color.GOLD))
 	cc.position.y = d/2
 	add_child(cc)
 
-	var cc2 = Global3d.new_torus(r*0.05, r*0.03, Global3d.get_color_mat(Color.RED))
+	var cc2 = Global3d.new_torus(r*0.1, r*0.06, Global3d.get_color_mat(Color.DARK_GOLDENROD))
 	cc2.position.y = d/2
 	add_child(cc2)
 
@@ -34,8 +36,8 @@ func add_bar(deg :float, r1 :float, r2 :float, w :float, h:float):
 	bar.position = Vector3(sin(rad)*(r1+r2)/2, h/2, cos(rad)*(r1+r2)/2)
 	add_child(bar)
 
-func add_text(deg :float, r :float, fsize :float, h:float, s :String):
-	var mat = Global3d.get_color_mat(Color.WHITE)
+func add_text(deg :float, r :float, fsize :float, h:float,co :Color, s :String):
+	var mat = Global3d.get_color_mat(co)
 	var rad = deg_to_rad(-deg+90)
 	var t = Global3d.new_text(fsize, h, mat, s)
 	t.rotation = Vector3(-PI/2,deg_to_rad(-deg),-PI/2)
