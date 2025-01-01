e78e323c538aff0dde355ecbc761b616f342ee5a
SeukWon Kang <kasworld@gmail.com>
2025-01-01 16:17:22 +0900

    한글로 정리

--- main.gd
+++ main.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	vp_size = get_viewport().get_visible_rect().size
 	sect_width = min(vp_size.x,vp_size.y)
 	var depth = sect_width/40
-	$Roulette.init(sect_width, depth, depth)
+	$Roulette.init(36, sect_width, depth)
 	$Roulette.position = Vector3(0,0,0)
 
 	$DirectionalLight3D.position = Vector3(0,sect_width,1)
--- roulette.gd
+++ roulette.gd
@@ -12,48 +12,54 @@ class 한칸:
 
 var 칸들 :Array[한칸]
 
-func init(r :float, d :float, fsize :float) -> void:
-	var plane = Global3d.new_cylinder(d, r,r, Global3d.get_color_mat(Color.DARK_GREEN ) )
-	plane.position.y = -d
-	add_child(plane)
+func init(칸수 :int, 반지름 :float, 깊이 :float) -> void:
+	배경원판만들기(반지름, 깊이, Color.DARK_GREEN)
+	중앙장식만들기(반지름, 깊이, Color.GOLD, Color.GOLDENROD)
+	칸들만들기(칸수, 반지름, 깊이, Color.WHITE)
+
+func 칸들만들기(칸수 :int, 반지름 :float, 깊이 :float, 선색깔 :Color) -> void:
 	칸들 = []
-	var n = 36
-	for i in n:
-		var unit = 360.0/n
+	for i in 칸수:
+		var unit = 360.0/칸수
 		var deg = unit * i
-		var l = add_bar(deg, r*0.5, r, d/10, d)
-		var co = Color.RED
+		var l = 칸나누는선만들기(deg, 반지름*0.5, 반지름, 깊이/10, 깊이, 선색깔)
+		var 글씨색깔 = Color.RED
 		if i % 2 == 0:
-			co = Color.BLUE
-		var t = add_text(deg+ unit/2, r*0.9, r/10, d, co, "%d" % [ i * 25 % n ] )
+			글씨색깔 = Color.BLUE
+		var t = 칸글씨만들기(deg+ unit/2, 반지름*0.9, 반지름/10, 깊이, 글씨색깔, "%d" % [ i * 25 % 칸수 ] )
 		칸들.append(한칸.new(deg,l,t))
 
-	var cc = Global3d.new_cylinder(d,r*0.04,r*0.04, Global3d.get_color_mat(Color.GOLD))
-	cc.position.y = d/2
-	add_child(cc)
+func 배경원판만들기(반지름 :float, 깊이 :float, 원판색깔 :Color) -> void:
+	var plane = Global3d.new_cylinder(깊이, 반지름, 반지름, Global3d.get_color_mat(원판색깔))
+	plane.position.y = -깊이
+	add_child(plane)
 
-	var cc2 = Global3d.new_torus(r*0.1, r*0.06, Global3d.get_color_mat(Color.DARK_GOLDENROD))
-	cc2.position.y = d/2
+func 중앙장식만들기(원판반지름 :float, 깊이 :float, 색깔1 :Color, 색깔2 :Color) -> void:
+	var cc = Global3d.new_cylinder(깊이, 원판반지름*0.04, 원판반지름*0.04, Global3d.get_color_mat(색깔1))
+	cc.position.y = 깊이/2
+	add_child(cc)
+	var cc2 = Global3d.new_torus(원판반지름*0.1, 원판반지름*0.06, Global3d.get_color_mat(색깔2))
+	cc2.position.y = 깊이/2
 	add_child(cc2)
 
-func add_bar(deg :float, r1 :float, r2 :float, w :float, h:float) -> MeshInstance3D:
-	w = max(1,w)
-	h = max(1,h)
-	var mat = Global3d.get_color_mat(Color.WHITE)
+func 칸나누는선만들기(deg :float, 시작거리 :float, 끝거리 :float, 폭 :float, 높이:float, 선색깔 :Color) -> MeshInstance3D:
+	폭 = max(1,폭)
+	높이 = max(1,높이)
+	var mat = Global3d.get_color_mat(선색깔)
 	var rad = deg_to_rad(-deg+90)
-	var bar_len = r2-r1
-	var bar_size = Vector3(bar_len,h,w)
+	var bar_len = 끝거리-시작거리
+	var bar_size = Vector3(bar_len, 높이,폭)
 	var bar = Global3d.new_box(bar_size, mat)
 	bar.rotation.y = deg_to_rad(-deg)
-	bar.position = Vector3(sin(rad)*(r1+r2)/2, h/2, cos(rad)*(r1+r2)/2)
+	bar.position = Vector3(sin(rad)*(시작거리+끝거리)/2, 높이/2, cos(rad)*(시작거리+끝거리)/2)
 	add_child(bar)
 	return bar
 
-func add_text(deg :float, r :float, fsize :float, h:float,co :Color, s :String) -> MeshInstance3D:
-	var mat = Global3d.get_color_mat(co)
+func 칸글씨만들기(deg :float, 중심부터거리 :float, 글씨크기 :float, 높이:float, 글씨색깔 :Color, 글내용 :String) -> MeshInstance3D:
+	var mat = Global3d.get_color_mat(글씨색깔)
 	var rad = deg_to_rad(-deg+90)
-	var t = Global3d.new_text(fsize, h, mat, s)
+	var t = Global3d.new_text(글씨크기, 높이, mat, 글내용)
 	t.rotation = Vector3(-PI/2,deg_to_rad(-deg),-PI/2)
-	t.position = Vector3(sin(rad)*r, h/2, cos(rad)*r)
+	t.position = Vector3(sin(rad)*중심부터거리, 높이/2, cos(rad)*중심부터거리)
 	add_child(t)
 	return t
