c15eb80285a89adce9427a33a2a1cadb3561d51f
SeukWon Kang <kasworld@gmail.com>
2025-01-03 11:52:16 +0900

    참가자  UI  추가

--- main.gd
+++ main.gd
@@ -7,7 +7,13 @@ func _ready() -> void:
 	vp_size = get_viewport().get_visible_rect().size
 	RenderingServer.set_default_clear_color( Global3d.colors.default_clear)
 
-	vp_size = get_viewport().get_visible_rect().size
+	var r = min(vp_size.x,vp_size.y)/2
+	$"왼쪽패널".size = Vector2(vp_size.x/2 -r, vp_size.y)
+	print($"왼쪽패널".size)
+	print($"왼쪽패널/참가자목록".size)
+	$오른쪽패널.size = Vector2(vp_size.x/2 -r, vp_size.y)
+	$오른쪽패널.position = Vector2(vp_size.x/2 + r, 0)
+
 	판반지름 = min(vp_size.x,vp_size.y)
 	var depth = 판반지름/40
 	$회전판.init(1, 판반지름, depth)
@@ -56,14 +62,24 @@ func _unhandled_input(event: InputEvent) -> void:
 		elif event.keycode == KEY_LEFT:
 			$회전판.rotation.y -= PI/180.0
 		elif event.keycode == KEY_INSERT:
-			var co = NamedColorList.color_list.pick_random()
-			$"회전판".칸추가하기(co[0],co[1])
-			$"회전판".칸위치정리하기()
+			참가자추가하기()
 		elif event.keycode == KEY_DELETE:
 			$"회전판".마지막칸지우기()
-
-
 
 func 회전판강조상태켜기() -> void:
 	var 선택칸 =  $"회전판".각도로칸선택하기(rad_to_deg($"회전판".rotation.y)+90)
 	선택칸.강조상태켜기()
+
+func 참가자추가하기() -> void:
+	var 현재칸수 = $"회전판".칸수얻기()
+	var 참가자 = TextEdit.new()
+	참가자.text = "참가자%d" % [현재칸수+1]
+	참가자.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	참가자.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	참가자.scroll_fit_content_height = true
+	참가자.custom_minimum_size.y = 50
+	$"왼쪽패널/참가자목록".add_child(참가자)
+	#$"왼쪽패널/참가자목록".custom_minimum_size.y = (현재칸수+1) * 50
+	var co = NamedColorList.color_list.pick_random()
+	$"회전판".칸추가하기(co[0],co[1])
+	$"회전판".칸위치정리하기()
