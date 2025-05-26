7bd94ecb4a38a8c48a7c70cefd246395ab6ee329
Kang Seukwon <kasworld@gmail.com>
2025-05-26 15:04:00 +0900

    칸 선, 글씨 정적 생성

--- 칸.gd
+++ 칸.gd
@@ -28,24 +28,20 @@ func init(각도 :float, 반지름a :float, 깊이a :float, 색깔a :Color, 글 
 func 선만들기()->void:
 	var 선폭 = max(1,깊이 /10)
 	깊이 = max(1,깊이)
-	var mesh = BoxMesh.new()
-	mesh.size = Vector3(반지름*(선끝비 - 선시작비), 깊이, 선폭)
-	mesh.material = Global3d.get_color_mat(색깔)
-	$"시작선".mesh = mesh
+	$"시작선".mesh.size = Vector3(반지름*(선끝비 - 선시작비), 깊이, 선폭)
+	$"시작선".mesh.material.albedo_color = 색깔
 	$"시작선".rotation.y = deg_to_rad(칸각도/2)
 	var rad = deg_to_rad(칸각도/2 + 90)
 	$"시작선".position = Vector3(sin(rad)*반지름*(선끝비 + 선시작비)/2, 깊이/2, cos(rad)*반지름*(선끝비 + 선시작비)/2)
 
 func 글씨만들기() -> void:
-	var mesh = TextMesh.new()
-	mesh.font = Global3d.font
-	mesh.depth = 깊이
-	mesh.pixel_size = 반지름 * 0.001
-	mesh.font_size = 반지름 * 0.07
-	mesh.text = 글내용
-	mesh.material = Global3d.get_color_mat(색깔)
-	mesh.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT
-	$"글씨".mesh = mesh
+	$"글씨".mesh.font = Global3d.font
+	$"글씨".mesh.depth = 깊이
+	$"글씨".mesh.pixel_size = 반지름 * 0.001
+	$"글씨".mesh.font_size = 반지름 * 0.07
+	$"글씨".mesh.text = 글내용
+	$"글씨".mesh.material.albedo_color = 색깔
+	$"글씨".mesh.horizontal_alignment = HorizontalAlignment.HORIZONTAL_ALIGNMENT_RIGHT
 	$"글씨".rotation = Vector3(-PI/2,PI/2,-PI/2)
 	$"글씨".position = Vector3(반지름, 깊이/2, 0)
 
