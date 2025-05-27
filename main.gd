1d9f80a4878fd54dbc04189b33e71f70e04f43a9
Kang Seukwon <kasworld@gmail.com>
2025-05-27 10:08:09 +0900

    update screenshot

--- main.gd
+++ main.gd
@@ -21,15 +21,15 @@ func _ready() -> void:
 	var n = 4
 	for i in n:
 		var rd = 2*PI/n *i
-		var r = 짧은길이
+		var r = 짧은길이*0.8
 		회전판추가(i)
-		회전판들[i].position = Vector3(sin(rd)*r,0,cos(rd)*r)
+		회전판들[i].position = Vector3(sin(rd)*r/1.3, 0, cos(rd)*r*1.3)
 	
 	reset_camera_pos()
 
 func 회전판추가(id :int) -> 회전판:
 	var rp = preload("res://회전판.tscn").instantiate().init(
-		id, 짧은길이*0.7, 짧은길이/40,
+		id, 짧은길이*0.6, 짧은길이/40,
 		NamedColorList.color_list.pick_random()[0],
 		NamedColorList.color_list.pick_random()[0],
 		NamedColorList.color_list.pick_random()[0],
