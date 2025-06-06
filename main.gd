49c76e2985835c581afe941f391e822bb450abc7
Kang Seukwon <kasworld@gmail.com>
2025-06-06 14:45:16 +0900

    자동으로다시돌리기 토글

--- main.gd
+++ main.gd
@@ -1,12 +1,11 @@
 extends Node3D
 
-# ♠♣♥♦
-
 var vp_size :Vector2
 var 짧은길이 :float
 var 회전판들 :Array
 var playing_card_deck :Array
 var deck_index :int
+var 자동으로다시돌리기 :bool = true
 func _ready() -> void:
 	vp_size = get_viewport().get_visible_rect().size
 	RenderingServer.set_default_clear_color( Global3d.colors.default_clear)
@@ -50,14 +49,15 @@ func 회전판추가(id :int) -> 회전판:
 			var co = NamedColorList.color_list.pick_random()
 			rp.칸추가하기(co[0], text_list[i] )
 	rp.칸위치정리하기()
-	if id == 0:
-		rp.rotation_stopped.connect(결과가결정됨)
+	rp.rotation_stopped.connect(결과가결정됨)
 	add_child(rp)
 	return rp
 	
 func 결과가결정됨(id :int) -> void:
-	print("결과" , 회전판들[0].선택된칸얻기())
-	$TimedMessage.show_message( "회전판%d 에서 %s (이)가 선택되었습니다." % [id, 회전판들[0].선택된칸얻기().글내용] ,3 )
+	if id == 0:
+		$TimedMessage.show_message( "회전판%d 에서 %s (이)가 선택되었습니다." % [id, 회전판들[id].선택된칸얻기().글내용] ,3 )
+	if 자동으로다시돌리기:
+		회전판들[id].돌리기시작.call_deferred(randfn(0, 5) )
 
 func reset_camera_pos()->void:
 	$Camera3D.position = Vector3(-1,max(vp_size.x,vp_size.y),0)
@@ -80,6 +80,7 @@ var key2fn = {
 	KEY_INSERT:_on_참가자추가_pressed,
 	KEY_DELETE:_on_참가자제거_pressed,
 	KEY_F1:_on_참가자숨기기_pressed,
+	KEY_F2:_on_자동돌리기_pressed,
 }
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
@@ -142,3 +143,6 @@ func _on_카메라변경_pressed() -> void:
 
 func _on_참가자숨기기_pressed() -> void:
 	$"왼쪽패널".visible = not $"왼쪽패널".visible
+
+func _on_자동돌리기_pressed() -> void:
+	자동으로다시돌리기 = not 자동으로다시돌리기
